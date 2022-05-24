cfbda8d60a1af3cf84a0e5b117703d1024022eb1
JamesH <jwhusc7@gmail.com>
2022-05-24 10:42:22 -0400

    added comments to controllers/index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
-
+// api routes
 const apiRoutes = require('./api/');
 
+// if api route use api folder
 router.use('/api', apiRoutes);
 
 module.exports = router;
