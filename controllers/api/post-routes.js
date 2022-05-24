f2a6ac84bb1552de5f81ce90bea311cd4367de7f
JamesH <jwhusc7@gmail.com>
2022-05-24 12:27:12 -0400

    created POST route for posts

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -78,19 +78,18 @@ router.get('/:id', (req, res) => {
     });
 });
 
-// router.post('/', withAuth, (req, res) => {
-//   // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
-//   Post.create({
-//     title: req.body.title,
-//     post_url: req.body.post_url,
-//     user_id: req.session.user_id
-//   })
-//     .then(dbPostData => res.json(dbPostData))
-//     .catch(err => {
-//       console.log(err);
-//       res.status(500).json(err);
-//     });
-// });
+router.post('/', withAuth, (req, res) => {
+  Post.create({
+    title: req.body.title,
+    post_url: req.body.post_url,
+    user_id: req.session.user_id
+  })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 // router.put('/upvote', withAuth, (req, res) => {
 //   // custom static method created in models/Post.js
