4813be314de3e7b8c773da3db2fea489546e81b1
JamesH <jwhusc7@gmail.com>
2022-05-25 13:51:58 -0400

    added GET route for comments

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 // api endpoints
 const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
 
 // uses end point modules to find route
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
