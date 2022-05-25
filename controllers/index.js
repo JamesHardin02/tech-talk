04533a19b466cf9703951298c332f7189851a425
JamesH <jwhusc7@gmail.com>
2022-05-25 18:19:01 -0400

    created dashboard and ability to create a new post

--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,12 @@ const router = require('express').Router();
 // routes
 const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
 
-// if '/' use homeroutes file
+// if '/' use home-routes.js file
 router.use('/', homeRoutes);
+// if /dashboard use dashboard-routes.js
+router.use('/dashboard', dashboardRoutes);
 // use end api endpoints folder if /api
 router.use('/api', apiRoutes);
 
