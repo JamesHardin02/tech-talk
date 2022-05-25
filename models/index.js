41d8656bfd116cafb7f965bf843f67ada4d89153
JamesH <jwhusc7@gmail.com>
2022-05-25 12:38:20 -0400

    created Vote model

--- models/index.js
+++ models/index.js
@@ -1,10 +1,11 @@
 // import all models
 const Post = require('./Post');
 const User = require('./User');
-// const Vote = require('./Vote');
+const Vote = require('./Vote');
 // const Comment = require('./Comment');
 
 // create associations
+// One to many relationship between user and post
 User.hasMany(Post, {
   foreignKey: 'user_id'
 });
@@ -14,38 +15,42 @@ Post.belongsTo(User, {
   onDelete: 'SET NULL'
 });
 
-// User.belongsToMany(Post, {
-//   through: Vote,
-//   as: 'voted_posts',
+// through table connection between user and post via vote
+User.belongsToMany(Post, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
 
-//   foreignKey: 'user_id',
-//   onDelete: 'SET NULL'
-// });
+Post.belongsToMany(User, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL'
+});
 
-// Post.belongsToMany(User, {
-//   through: Vote,
-//   as: 'voted_posts',
-//   foreignKey: 'post_id',
-//   onDelete: 'SET NULL'
-// });
+// one to one relationship between a vote 
+// and a user and a vote and a post
+Vote.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
 
-// Vote.belongsTo(User, {
-//   foreignKey: 'user_id',
-//   onDelete: 'SET NULL'
-// });
+Vote.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL'
+});
 
-// Vote.belongsTo(Post, {
-//   foreignKey: 'post_id',
-//   onDelete: 'SET NULL'
-// });
+// many to many relationship between user and votes
+// and post and votes.
+User.hasMany(Vote, {
+  foreignKey: 'user_id'
+});
 
-// User.hasMany(Vote, {
-//   foreignKey: 'user_id'
-// });
-
-// Post.hasMany(Vote, {
-//   foreignKey: 'post_id'
-// });
+Post.hasMany(Vote, {
+  foreignKey: 'post_id'
+});
 
 // Comment.belongsTo(User, {
 //   foreignKey: 'user_id',
@@ -67,4 +72,4 @@ Post.belongsTo(User, {
 // });
 
 //module.exports = { User, Post, Vote, Comment };
-module.exports = { User, Post };
+module.exports = { User, Post, Vote };
